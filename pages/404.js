11fac8fd02a63c9c145d0be30b0f3af0cc0aab9b
masachi <zhangjb42@gmail.com>
2022-08-31 12:40:07 +0800

    fix: 修复图片路径错误

--- pages/404.js
+++ pages/404.js
@@ -10,7 +10,7 @@ export default function FourZeroFour() {
       </div>
       <div className="max-w-md">
         <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">找不到了的说~</p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-8">不用担心，还有其他好东西可以看.jpg</p>
         <Link href="/">
           <button className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
             回到首页

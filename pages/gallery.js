9862a22310bfc9b709f3eb4a3a9509b4843924dc
masachi <zhangjb42@gmail.com>
2022-09-13 13:26:34 +0800

    feature: timeline

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,35 +1,69 @@
 import React, { useRef, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
-import fs from 'fs'
-import path from 'path'
-import { MasonryInfiniteGrid } from '@egjs/react-infinitegrid'
 import { PageSEO } from '@/components/SEO'
 import { PhotoSlider } from 'react-photo-view'
 import { Timeline } from 'antd'
 import PhotoAlbum from 'react-photo-album'
+import { Octokit } from 'octokit'
+import dayjs from 'dayjs'
 
-const root = process.cwd()
-const galleryPath = 'public/static/assets/gallery'
+const GIST_TOKEN = process.env.GIST_TOKEN
+
+const octokit = new Octokit({
+  auth: GIST_TOKEN,
+})
 
 export async function getStaticProps() {
-  const images = fs.readdirSync(path.join(root, galleryPath))
+  const gistContent = await getContentByGistId(process.env.GIST_ID, process.env.FILE_NAME)
+  const album = []
+  const images = []
+  Object.keys(gistContent)
+    .sort((dateA, dateB) => dayjs(dateB).unix() - dayjs(dateA).unix())
+    .forEach((date) => {
+      album.push({
+        date: date,
+        thumbnails: gistContent[date]
+          .filter((items) => items.length > 0)
+          .map((items) => {
+            let item = items[items.length >= 2 ? items.length - 2 : items[0]]
+            return {
+              ...item,
+              url: `${process.env.IMG_DOMAIN}${item.path}`,
+              src: `${process.env.IMG_DOMAIN}${item.path}`,
+            }
+          }),
+      })
+
+      images.push(
+        gistContent[date]
+          .filter((items) => items.length > 0)
+          .map((items) => {
+            let item = items[items.length - 1]
+            return {
+              src: `${process.env.IMG_DOMAIN}${item.path}`,
+              id: item.id,
+            }
+          })
+      )
+    })
+
   return {
     props: {
-      images: images.map((item) => {
-        return {
-          fileName: item,
-          id: uuidv4(),
-          height: 200,
-          width: 200,
-          url: `/${galleryPath.replace('public/', '')}/${item}`,
-          src: `/${galleryPath.replace('public/', '')}/${item}`,
-        }
-      }),
+      albums: album,
+      images: images,
     },
   }
 }
 
-export default function Gallery({ images }) {
+const getContentByGistId = async (gist_id, fileName) => {
+  const gistGetResponse = await octokit.request(`GET /gists/${gist_id}`)
+  if (gistGetResponse.status === 200) {
+    return JSON.parse(gistGetResponse.data.files[fileName].content)
+  }
+
+  return {}
+}
+
+export default function Gallery({ albums, images }) {
   const [previewModalOpen, setPreviewModalOpen] = useState(false)
   const [previewIndex, setPreviewIndex] = useState(0)
 
@@ -44,36 +78,22 @@ export default function Gallery({ images }) {
   return (
     <>
       <PageSEO title={`个人收藏沙雕图`} description={`个人收藏沙雕图`} />
-      {/*<MasonryInfiniteGrid className="container" gap={5} align={'justify'}>*/}
-      {/*  {images.map((item) => {*/}
-      {/*    return (*/}
-      {/*      <div className="item" key={item.id}>*/}
-      {/*        <div className="thumbnail">*/}
-      {/*          <img*/}
-      {/*            onClick={() => {*/}
-      {/*              handlePreview(item)*/}
-      {/*            }}*/}
-      {/*            data-grid-lazy="true"*/}
-      {/*            src={`${item.url}`}*/}
-      {/*            alt={''}*/}
-      {/*          />*/}
-      {/*        </div>*/}
-      {/*      </div>*/}
-      {/*    )*/}
-      {/*  })}*/}
-      {/*</MasonryInfiniteGrid>*/}
 
       <div className={'gallery-container'}>
         <Timeline mode={'left'}>
-          <Timeline.Item label="2015-09-01">
-            <PhotoAlbum layout="rows" photos={images.slice(0, 10)} />
-          </Timeline.Item>
-          <Timeline.Item label="2015-09-01">
-            <PhotoAlbum layout="rows" photos={images.slice(10, 20)} />
-          </Timeline.Item>
-          <Timeline.Item label="2015-09-01">
-            <PhotoAlbum layout="rows" photos={images.slice(20, 30)} />
-          </Timeline.Item>
+          {albums.map((dateImages) => {
+            return (
+              <Timeline.Item key={dateImages.date} label={dateImages.date}>
+                <PhotoAlbum
+                  layout="rows"
+                  photos={dateImages.thumbnails}
+                  onClick={(event, photo, index) => {
+                    handlePreview(photo)
+                  }}
+                />
+              </Timeline.Item>
+            )
+          })}
         </Timeline>
       </div>
 

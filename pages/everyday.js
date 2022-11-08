e56a6f8f8fb67cb6969f4c972c62b2034111e0d7
HOME\sdlds <zhangjb42@gmail.com>
2022-11-08 21:24:58 +0800

    feature: fbi warning

--- pages/everyday.js
+++ pages/everyday.js
@@ -2,6 +2,10 @@ import { getAllFilesFrontMatter } from '@/lib/mdx'
 import siteMetadata from '@/data/siteMetadata'
 import EverydayListLayout from '@/layouts/EverydayListLayout'
 import { PageSEO } from '@/components/SEO'
+import { useEffect, useState } from 'react'
+import { Modal, Spin } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons'
+import Link from '../components/Link'
 
 export const EVERYDAYS_PER_PAGE = 5
 
@@ -17,15 +21,64 @@ export async function getStaticProps() {
 }
 
 export default function Everyday({ everydays, initialDisplayEverydays, pagination }) {
+  const [shouldDisplayPorn, setShouldDisplayPorn] = useState(undefined)
+
+  const renderWarningContent = () => {
+    return (
+      <div className="porn-container">
+        <div className="porn-warning-container">
+          <p className="warning">FBI Warning</p>
+          <p className="warning">请注意</p>
+          <p>以下内容可能涉及成人内容</p>
+          <p>请不要在工作时间或者是公共场合观看以下内容</p>
+          <p>未满18岁的未成年人请在成人的陪同下观看</p>
+
+          <div className="btn-container">
+            <span
+              className="porn-confirm-btn"
+              onClick={() => {
+                localStorage.setItem('porn-display', new Date().getTime())
+                setShouldDisplayPorn(true)
+              }}
+            >
+              继续
+            </span>
+            <Link href="/" aria-label={siteMetadata.headerTitle} className="porn-confirm-btn">
+              返回首页
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  useEffect(() => {
+    let timestamp = localStorage.getItem('porn-display') || 0
+    if (new Date().getTime() - parseInt(timestamp) <= 7 * 24 * 60 * 60 * 1000) {
+      setShouldDisplayPorn(true)
+    } else {
+      localStorage.removeItem('porn-display')
+      setShouldDisplayPorn(false)
+    }
+  }, [])
+
   return (
     <>
-      <PageSEO title={`Everyday - ${siteMetadata.author}`} description={siteMetadata.description} />
-      <EverydayListLayout
-        posts={everydays}
-        initialDisplayPosts={initialDisplayEverydays}
-        pagination={pagination}
-        title="每日一冲"
-      />
+      <PageSEO title={`每日一冲 - ${siteMetadata.author}`} description={siteMetadata.description} />
+      {shouldDisplayPorn == undefined ? (
+        <div className="loading-container">
+          <Spin indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />} />
+        </div>
+      ) : shouldDisplayPorn === true ? (
+        <EverydayListLayout
+          posts={everydays}
+          initialDisplayPosts={initialDisplayEverydays}
+          pagination={pagination}
+          title="每日一冲"
+        />
+      ) : (
+        renderWarningContent()
+      )}
     </>
   )
 }
--- pages/everyday/[...slug].js
+++ pages/everyday/[...slug].js
@@ -1,6 +1,11 @@
 import PageTitle from '@/components/PageTitle'
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { formatSlug, getAllFilesFrontMatter, getFileBySlug, getFiles } from '@/lib/mdx'
+import { useEffect, useState } from 'react'
+import { Modal, Spin } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons'
+import Link from '../../components/Link'
+import siteMetadata from '@/data/siteMetadata'
 
 const DEFAULT_LAYOUT = 'EverydayPostLayout'
 
@@ -43,18 +48,62 @@ export async function getStaticProps({ params }) {
 export default function Blog({ everyday, authorDetails, prev, next }) {
   const { mdxSource, toc, frontMatter } = everyday
 
+  const [shouldDisplayPorn, setShouldDisplayPorn] = useState(undefined)
+
+  const renderWarningContent = () => {
+    return (
+      <div className="porn-container">
+        <div className="porn-warning-container">
+          <p className="warning">请注意</p>
+          <p>以下内容可能涉及成人内容</p>
+          <p>请不要在工作时间或者是公共场合观看以下内容</p>
+          <p>未满18岁的未成年人请在成人的陪同下观看</p>
+
+          <div className="btn-container">
+            <span
+              className="porn-confirm-btn"
+              onClick={() => {
+                localStorage.setItem('porn-display', new Date().getTime())
+                setShouldDisplayPorn(true)
+              }}
+            >
+              继续
+            </span>
+            <Link href="/" aria-label={siteMetadata.headerTitle} className="porn-confirm-btn">
+              返回首页
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  useEffect(() => {
+    let timestamp = localStorage.getItem('porn-display') || 0
+    if (new Date().getTime() - parseInt(timestamp) <= 7 * 24 * 60 * 60 * 1000) {
+      setShouldDisplayPorn(true)
+    } else {
+      localStorage.removeItem('porn-display')
+      setShouldDisplayPorn(false)
+    }
+  }, [])
+
   return (
     <>
       {frontMatter.draft !== true ? (
-        <MDXLayoutRenderer
-          layout={frontMatter.layout || DEFAULT_LAYOUT}
-          toc={toc}
-          mdxSource={mdxSource}
-          frontMatter={frontMatter}
-          authorDetails={authorDetails}
-          prev={prev}
-          next={next}
-        />
+        shouldDisplayPorn === true ? (
+          <MDXLayoutRenderer
+            layout={frontMatter.layout || DEFAULT_LAYOUT}
+            toc={toc}
+            mdxSource={mdxSource}
+            frontMatter={frontMatter}
+            authorDetails={authorDetails}
+            prev={prev}
+            next={next}
+          />
+        ) : (
+          renderWarningContent()
+        )
       ) : (
         <div className="mt-24 text-center">
           <PageTitle>
